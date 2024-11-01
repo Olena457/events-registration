12d47f457454bbe29e188fee17214932ff56b1c2
Olena457 <olenademianenko42@gmail.com>
2024-11-01 22:23:26 +0200

    updeted home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './Home.module.css';
+import ContainerWrapper from '../../components/ContainerWrapper/ContainerWrapper.jsx';
 
 const Home = () => {
   return (
-    <div>
+    <ContainerWrapper>
       <h4 className={css.title}> Hello user!Welcome to the event board!</h4>
-      <p className={css.message}>
-        Create your event now!
-        <br />
-        Choose the event you watn to attend!
-      </p>
-      <NavLink to="/events/:id" className={css.btnGo}>
-        Go to Events
-      </NavLink>
-      <NavLink to="/create-event" className={css.btnCreate}>
-        Create Event
-      </NavLink>
-    </div>
+      <div className={css.contWrapper}>
+        <p className={css.message}>
+          Create your event now!
+          <br />
+          Choose the event you watn to attend!
+        </p>
+      </div>
+      <div className={css.buttonWrapper}>
+        <NavLink to="/events/:id" className={css.btnGo}>
+          Go Events
+        </NavLink>
+        <NavLink to="/create-event" className={css.btnCreate}>
+          Create Event
+        </NavLink>
+      </div>
+    </ContainerWrapper>
   );
 };
 
