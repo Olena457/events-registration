c41eed376d15f4ab3e0e641942e3e2489d4b7253
Olena457 <olenademianenko42@gmail.com>
2025-01-18 21:22:40 +0200

    created data.json

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,31 @@
-// import React from 'react';
 // import { Link } from 'react-router-dom';
 // import styles from './Card.module.css';
 
-// export default function Card({ title, description, id, organizer, card_date }) {
+// export default function Card({ card }) {
+//   const isLoggedIn = useSelector(selectIsLoggedIn);
+//   // const dispatch = useDispatch();
+//   // const [isRegister, setRegister] = useState(false);
+//   if (!isLoggedIn) {
+//     toast.info('Login first to view participants!', {
+//       position: 'top-center',
+//     });
+//     return null;
+//   }
 //   return (
 //     <div className={styles.cardContainer}>
 //       <div className={styles.cardInfo}>
 //         <p className={styles.cardName}>Title: {title}</p>
 //       </div>
 //       <div className={styles.cardInfo}>
-//         <p className={styles.cardDescription}>Description: {description}</p>
+//         <img
+//           src={organizerImage}
+//           alt={`Organizer ${organizer}`}
+//           className={styles.organizerImage}
+//         />
+//         <p className={styles.cardName}>Organizer: {organizer}</p>
 //       </div>
 //       <div className={styles.cardInfo}>
-//         <p className={styles.cardName}>Organizer: {organizer}</p>
+//         <p className={styles.cardDescription}>Description: {description}</p>
 //       </div>
 //       <div className={styles.cardInfo}>
 //         <p className={`${styles.cardDate} ${styles.cardName}`}>
@@ -32,14 +45,16 @@
 //     </div>
 //   );
 // }
-
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import styles from './Card.module.css';
 
 export default function Card({ card }) {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  // const dispatch = useDispatch();
-  // const [isRegister, setRegister] = useState(false);
+  const { title, description, date, organizer } = card;
+  const organizerImage = card.organizer.imageUrl;
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
   if (!isLoggedIn) {
     toast.info('Login first to view participants!', {
       position: 'top-center',
@@ -52,37 +67,32 @@ export default function Card({ card }) {
         <p className={styles.cardName}>Title: {title}</p>
       </div>
       <div className={styles.cardInfo}>
-        <img
-          src={organizerImage}
-          alt={`Organizer ${organizer}`}
-          className={styles.organizerImage}
-        />
-        <p className={styles.cardName}>Organizer: {organizer}</p>
-      </div>
-      <div className={styles.cardInfo}>
         <p className={styles.cardDescription}>Description: {description}</p>
       </div>
       <div className={styles.cardInfo}>
         <p className={`${styles.cardDate} ${styles.cardName}`}>
-          Date: {card_date}
+          Date: {new Date(date).toLocaleString()}
         </p>
       </div>
+      <div className={styles.cardInfo}>
+        <img
+          src={organizerImage}
+          alt={`Organizer ${organizer.full_name}`}
+          className={styles.organizerImage}
+        />
+        <p className={styles.cardName}>Organizer: {organizer.full_name}</p>
+      </div>
+
       <div className={styles.btnContainer}>
-        <Link to={`/cards/${id}/register`} className={styles.btn}>
+        <Link to={`/cards/${card.id}/register`} className={styles.btn}>
           Registration
         </Link>
       </div>
       <div className={styles.btnContainer}>
-        <Link to={`/cards/${id}/participants`} className={styles.btn}>
+        <Link to={`/cards/${card.id}/participants`} className={styles.btn}>
           View
         </Link>
       </div>
     </div>
   );
 }
-//  title,
-// description,
-// id,
-// organizer,
-// card_date,
-// organizerImage,
