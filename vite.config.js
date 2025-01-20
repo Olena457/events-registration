972794a4a6658a00ce1ecad87ddf498174364c2b
Olena457 <olenademianenko42@gmail.com>
2025-01-20 21:35:03 +0200

    udated json

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      external: ['REAme.md'],
-    },
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  server: {
-    mimeTypes: {
-      'text/jsx': 'text/javascript',
-    },
-  },
 });
